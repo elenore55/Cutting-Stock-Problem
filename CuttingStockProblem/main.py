ab3d6b3b0707f04aa252a298220aba131e4821f2
Milica Popovic <102756159+elenore55@users.noreply.github.com>
2023-03-31 22:18:07 +0200

    Implemented fitness function

--- CuttingStockProblem/main.py
+++ CuttingStockProblem/main.py
@@ -5,6 +5,7 @@ from random import randint, shuffle  # inclusive range
 L = 10
 l_arr = [4, 3, 2]
 d_arr = [50, 30, 35]
+S = sum([l_arr[i] * d_arr[i] for i in range(len(l_arr))])
 N = 3
 CHROMOSOME_LEN = 2 * N
 POPULATION_SIZE = 15
@@ -64,10 +65,27 @@ def initialize_population(max_repeat_arr):
     return init_population
 
 
+def evaluate_fitness(chromosome, patterns_arr):
+    P = 1
+    unsupplied_sum = 0
+    l_provided = [0] * len(l_arr)
+    for i in range(0, len(chromosome), 2):
+        pattern = patterns_arr[chromosome[i]]
+        for j in range(len(pattern)):
+            l_provided[j] += pattern[j] * chromosome[i + 1]
+    for i in range(len(l_arr)):
+        num_unsupplied = d_arr[i] - l_provided[i]
+        if num_unsupplied > 0:
+            unsupplied_sum += num_unsupplied * l_arr[i]
+    x_sum = L * sum(chromosome[i] for i in range(1, len(chromosome), 2))
+    return S / (x_sum + P * unsupplied_sum)
+
+
 if __name__ == '__main__':
     patterns = generate_efficient_patterns()
     max_repeat = calculate_max_pattern_repetition(patterns)
     initial_population = initialize_population(max_repeat)
     print(patterns)
     print(max_repeat)
-    print(initial_population)
+    # print(initial_population)
+    print(evaluate_fitness(initial_population[0], patterns))
